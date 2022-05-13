ae7600564c960c76894a390ec12d4d6419cbafda
Yueyang Huang <yueyang@yueyangs-mbp.dynamic.ucsd.edu>
2022-05-13 09:38:51 +0800

    completed all parts of lab1

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -14,6 +14,19 @@ public class LeapYear {
         }
     }
 
+    /** Checks whether a given year is Leap year
+      * @param year to be analyzed
+      */
+      public static boolean isLeapYear(int year){
+        if (year % 400 == 0) {
+          return true;
+        }
+        else if (year % 4 == 0 && year % 100 != 0){
+          return true;
+        }
+        return false;
+      }
+
     /** Must be provided an integer as a command line argument ARGS. */
     public static void main(String[] args) {
         if (args.length < 1) {
@@ -30,4 +43,3 @@ public class LeapYear {
         }
     }
 }
-
